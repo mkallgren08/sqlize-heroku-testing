6f728e94de5ee4e2a9c1b03c6753647cb31f62b9
mkallgren08 <mkallgren08@gmail.com>
2019-03-17 00:49:41 -0400

    Added in a simple res.send to check connection

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ sequelize
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.send("We good");
 });
 
 app.listen(PORT, function () {
